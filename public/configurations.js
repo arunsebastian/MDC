0996b70daed83cbebe7405f44344385d372fae51
Arun Sebastian <sebasta1@aklc.govt.nz>
2021-11-08 12:42:22 +1300

    added react-tabs, updated webmap for demo

--- public/configurations.js
+++ public/configurations.js
@@ -3,7 +3,8 @@ var EXTERNAL_CONFIG = {
 	webmapConfig: {
 		// "id": "6dcd087d08984049bdb4f30a9a4266d6",
 		// "portal": "https://demo.gbs.kiwi/portal/",
-		"id":"695a783e9c804309b1250f7e113e355b",
+		"id":"9a0c5eb10ea54a7ca4beef87b4c68b45",
+		//"id":"695a783e9c804309b1250f7e113e355b",
 		"portal":"https://gbs.maps.arcgis.com"
 	},
 	//
